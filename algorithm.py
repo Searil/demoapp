37077ed2189cc2aa55ee59747d92613627a3de85
BatuhanBardak <bbardak37@gmail.com>
2016-10-13 18:11:51 +0300

    Making last changes on algorithm and unit tast

--- algorithm.py
+++ algorithm.py
@@ -2,9 +2,12 @@ def append_and_sort(array, number1, number2):
 	# check if parameters stored in array
 	# if not append and sort
 
-    if number1 not in array:
-        array.append(number1)
-    if number2 not in array:
-        array.append(number2)
 
-    return sorted(array)
+    if number1 not in array and number1 is not None:
+        array.append(number1)
+    if number2 not in array and number2 is not None:
+        array.append(number2)
+    if len(array) == 0:
+    	return False
+    else:
+    	return sorted(array)
--- test_algorithm.py
+++ test_algorithm.py
@@ -2,11 +2,26 @@ import unittest
 from algorithm import append_and_sort
 
 class TestSearch(unittest.TestCase):
-    def setUp(self):
-        self.array = [3,2]
+	def setUp(self):
+		self.array = [3,2]
 
-    def test_successful(self):
-        self.assertEqual(append_and_sort(self.array,1,5),[1,2,3,5])
+	def test_successful(self):
+		self.assertEqual(append_and_sort(self.array,1,5),[1,2,3,5])
+	
+	
+	def test_whenArrayIsNone(self):
+		self.assertFalse(None, 10)
+	
+	def test_empty_same_parameters(self):
+		self.assertTrue(append_and_sort(self.array,0,0),[0,2,3])
 
+	
+	def test_emptyArray(self):
+		self.assertFalse(append_and_sort([], None,None))
+
+	def test_emptyToFull(self):
+		self.assertEqual(append_and_sort([],5,10), [5,10])
+	
+	
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
